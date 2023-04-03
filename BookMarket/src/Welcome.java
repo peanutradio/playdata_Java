c02483f2fef63f2009261440188ce49cd7f7381d
peanutradio <114959220+peanutradio@users.noreply.github.com>
2023-04-03 22:59:36 +0900

    Add files via upload

--- BookMarket/src/Welcome.java
+++ BookMarket/src/Welcome.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Welcome {
 	static final int NUM_BOOK = 3; // 자바의 상수는 static final(상수: 값 못바꿈)
 	static final int NUM_ITEM = 7; // 자바의 상수는 static final(상수: 값 못바꿈)
-
+    static CartItem[] mCartItem = new CartItem[NUM_BOOK];
+    static int mCartCount = 0;
+	
 	public static void main(String[] args) {
 		String[][] mBook = new String[NUM_BOOK][NUM_ITEM];
 
@@ -92,6 +94,17 @@ public class Welcome {
 
 	public static void menuCartItemList() {
 		System.out.println("2. 장바구니 상품 목록 보기 :");
+		System.out.println("-------------------------------------------");
+		System.out.println("    도서ID \t|       수량|        합계");
+		for(int i=0; i<mCartCount; i++) {
+			System.out.print("    "+ mCartItem[i].getBookID() + "\t|");
+			System.out.print("    "+ mCartItem[i].getQuantity() + "\t|");
+			System.out.print("    "+ mCartItem[i].getTotalPrice() + "\t|");
+			System.out.println("    ");
+		}
+		
+		
+		
 	}
 
 	public static void menuCartClear() {
@@ -103,8 +116,8 @@ public class Welcome {
 
 		BookList(book);
 		for (int i = 0; i < NUM_BOOK; i++) {
-			for (int j = 0; j < NUM_BOOK; j++)
-				System.out.println(book[i][j] + "|");
+			for (int j = 0; j < NUM_ITEM; j++)
+				System.out.print(book[i][j] + "|");
 			System.out.println(" ");
 		}
 		boolean quit = false;
@@ -131,6 +144,9 @@ public class Welcome {
 
 				if (str.toUpperCase().equals("Y")) {
 					System.out.println(book[numId][0] + "도서가 장바구니에 추가되었습니다.");
+				// 장바구니에 넣기
+				if (!isCartInBook(book[numId][0]))
+					mCartItem[mCartCount++]= new CartItem(book[numId]);
 				}
 				quit = true;
 			} else
@@ -157,14 +173,14 @@ public class Welcome {
 	public static void BookList(String[][] book) {
 
 		book[0][0] = "ISBN1234";
-		book[0][1] = "쉽게 배우는 JSP 웹 프로그래";
+		book[0][1] = "쉽게 배우는 JSP 웹 프로그래밍";
 		book[0][2] = "27000";
 		book[0][3] = "송미";
-		book[0][4] = "단계별로 쇼핑몰을 구현하며 배우는 JSP 웹 프로그래";
+		book[0][4] = "단계별로 쇼핑몰을 구현하며 배우는 JSP 웹 프로그래밍";
 		book[0][5] = "2018/10/08";
 
 		book[1][0] = "ISBN1235";
-		book[1][1] = "안드로이드 프로그래";
+		book[1][1] = "안드로이드 프로그래밍";
 		book[1][2] = "33000";
 		book[1][3] = "우재님";
 		book[1][4] = "실습 단계별 명쾌한 멘토";
@@ -174,11 +190,20 @@ public class Welcome {
 		book[2][0] = "ISBN1236";
 		book[2][1] = "스크래";
 		book[2][2] = "22000";
-		book[2][3] = "고광";
+		book[2][3] = "고광일";
 		book[2][4] = "컴퓨팅 사고력을 키우는 블록 코딩";
-		book[2][5] = "컴퓨터입";
+		book[2][5] = "컴퓨터입문";
 		book[2][6] = "2019/06/10";
-
+	}
+	public static boolean isCartInBook(String bookId) {
+		boolean flag = false;
+		for (int i=0; i<mCartCount; i++) {
+			if (bookId.equals(mCartItem[i].getBookID())) {
+				mCartItem[i].setQuantity(mCartItem[i].getQuantity()+1);
+				flag = true;
+			}
+		}
+		return flag;
 	}
 
 }
